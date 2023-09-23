e14f9e034c896f3ca255d014572fac4db6f3c782
anmoljamespilley <144140402+anmoljamespilley@users.noreply.github.com>
2023-09-23 23:16:21 +0530

    Add files via upload

---
+++ HrDepartment.java
@@ -0,0 +1,18 @@
+// HrDepartment class
+class HrDepartment extends SuperDepartment {
+    public String departmentName() {
+        return "HR Department";
+    }
+
+    public String getTodaysWork() {
+        return "Fill today's timesheet and mark your attendance";
+    }
+
+    public String getWorkDeadline() {
+        return "Complete by EOD";
+    }
+
+    public String doActivity(String activity) {
+        return activity;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,24 @@
+// Driver class
+public class Main {
+    public static void main(String[] args) {
+        AdminDepartment adminDept = new AdminDepartment();
+        System.out.println("Welcome to " + adminDept.departmentName());
+        System.out.println(adminDept.getTodaysWork());
+        System.out.println(adminDept.getWorkDeadline());
+        System.out.println(adminDept.isTodayAHoliday());
+
+        HrDepartment hrDept = new HrDepartment();
+        System.out.println("\nWelcome to " + hrDept.departmentName());
+        System.out.println(hrDept.doActivity("team Lunch"));
+        System.out.println(hrDept.getTodaysWork());
+        System.out.println(hrDept.getWorkDeadline());
+        System.out.println(hrDept.isTodayAHoliday());
+
+        TechDepartment techDept = new TechDepartment();
+        System.out.println("\nWelcome to " + techDept.departmentName());
+        System.out.println(techDept.getTodaysWork());
+        System.out.println(techDept.getWorkDeadline());
+        System.out.println(techDept.getTechStackInformation());
+        System.out.println(techDept.isTodayAHoliday());
+    }
+}
